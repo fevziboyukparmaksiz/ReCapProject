f5cebc2e678a51dc660fad44bb587f495b02a28a
Fevzi Böyükparmaksız <fevzibparmaksiz.gmail.comgit config --global user.email fevzibparmaksiz.gmail.comgit config --global user.name Fevzi>
2022-11-30 15:37:28 +0300

    EntityFramework

---
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfColorDal : IColorDal
+    {
+        public void Add(Color entity)
+        {
+            using (ReCapProjectDbContext context = new ReCapProjectDbContext())
+            {
+                var added = context.Entry(entity);
+                added.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Color entity)
+        {
+            using (ReCapProjectDbContext context = new ReCapProjectDbContext())
+            {
+                var deleted = context.Entry(entity);
+                deleted.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Color Get(Expression<Func<Color, bool>> filter)
+        {
+            using (ReCapProjectDbContext context = new ReCapProjectDbContext())
+            {
+                return context.Set<Color>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
+        {
+            using (ReCapProjectDbContext context = new ReCapProjectDbContext())
+            {
+                return filter==null
+                    ? context.Set<Color>().ToList()
+                    : context.Set<Color>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Color entity)
+        {
+            using (ReCapProjectDbContext context = new ReCapProjectDbContext())
+            {
+                var updated = context.Entry(entity);
+                updated.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,9 +16,9 @@ namespace DataAccess.Concrete.InMemory
         {
             _car= new List<Car>
             {
-                new Car { CarId = 1, BrandId= 1, ColorId = 1, DailyPrice=15000, Description="Honda", ModelYear = 2013 },
-                new Car { CarId = 2, BrandId= 2, ColorId = 2, DailyPrice=35000, Description="Ferrari", ModelYear = 2016 },
-                new Car { CarId = 3, BrandId= 3, ColorId = 3, DailyPrice=10000, Description="BMW", ModelYear = 2019 }
+                new Car { Id = 1, BrandId= 1, ColorId = 1, DailyPrice=15000, Description="Honda", ModelYear = 2013 },
+                new Car { Id = 2, BrandId= 2, ColorId = 2, DailyPrice=35000, Description="Ferrari", ModelYear = 2016 },
+                new Car { Id = 3, BrandId= 3, ColorId = 3, DailyPrice=10000, Description="BMW", ModelYear = 2019 }
             };
 
         }
@@ -30,13 +31,13 @@ namespace DataAccess.Concrete.InMemory
 
         public void Delete(Car car)
         {
-            Car carToDelete = _car.SingleOrDefault(c=> c.CarId == car.CarId);
+            Car carToDelete = _car.SingleOrDefault(c=> c.Id == car.Id);
             _car.Remove(carToDelete);
         }
         public void Update(Car car)
         {
-            Car carToDelete = _car.SingleOrDefault(c => c.CarId == car.CarId);
-            carToDelete.CarId = car.CarId;
+            Car carToDelete = _car.SingleOrDefault(c => c.Id == car.Id);
+            carToDelete.Id = car.Id;
             carToDelete.BrandId = car.BrandId;
             carToDelete.ColorId = car.ColorId;
             carToDelete.DailyPrice = car.DailyPrice;
@@ -51,9 +52,17 @@ namespace DataAccess.Concrete.InMemory
 
         public List<Car> GetById(int carId)
         {
-            return _car.Where(c=>c.CarId== carId).ToList();
+            return _car.Where(c=>c.Id== carId).ToList();
         }
 
-       
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
